ebdf75be33d524c041c538c528bc1c4d93cda3e6
Victor Arowo <arowovici@gmail.com>
2020-05-28 11:27:46 +0100

    Mock Responses for API Requests

---
+++ src/__mocks__/temperatureAndTimeResponse.ts
@@ -0,0 +1,43 @@
+export default {
+  coord: {
+    lon: -0.13,
+    lat: 51.51,
+  },
+  weather: [
+    {
+      id: 802,
+      main: "Clouds",
+      description: "scattered clouds",
+      icon: "03d",
+    },
+  ],
+  base: "stations",
+  main: {
+    temp: 288.05,
+    feels_like: 283.8,
+    temp_min: 287.04,
+    temp_max: 288.71,
+    pressure: 1036,
+    humidity: 67,
+  },
+  visibility: 10000,
+  wind: {
+    speed: 5.7,
+    deg: 90,
+  },
+  clouds: {
+    all: 40,
+  },
+  dt: 1590652004,
+  sys: {
+    type: 1,
+    id: 1414,
+    country: "GB",
+    sunrise: 1590637940,
+    sunset: 1590696221,
+  },
+  timezone: 3600,
+  id: 2643743,
+  name: "London",
+  cod: 200,
+};
